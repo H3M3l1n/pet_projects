b101410df2a86944ab37fad1340fc3f56e75425a
Andrew <hemelin23@gmail.com>
2024-04-13 17:05:12 +0300

    namebar + validation edit

--- react-chat/src/components/NameBar/NameBar.jsx
+++ react-chat/src/components/NameBar/NameBar.jsx
@@ -1,7 +1,92 @@
+import { useEffect, useState } from "react";
+import {
+  ClickAwayListener,
+  IconButton,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Check, Edit } from "@mui/icons-material";
 import "./namebar.sass";
 
 const NameBar = () => {
-  return <div className="name_bar"></div>;
+  const [userName, setUserName] = useState("");
+  const [editUserName, setEditUserName] = useState(false);
+  const [validUserName, setValidUserName] = useState(false);
+
+  const savedUserName = localStorage.getItem("chat_name");
+  const handlerSaveName = () => {
+    if (validUserName === true) {
+      setEditUserName(false);
+      localStorage.setItem("chat_name", userName);
+    } else return;
+  };
+  const handlerEditName = () => {
+    setEditUserName(true);
+  };
+
+  console.log(savedUserName);
+
+  useEffect(() => {
+    if (savedUserName && savedUserName !== null) {
+      setUserName(savedUserName);
+    } else setEditUserName(true);
+  }, [savedUserName]);
+
+  useEffect(() => {
+    if (userName.length >= 3) {
+      setValidUserName(true);
+    } else setValidUserName(false);
+    console.log(userName.length);
+    console.log(validUserName);
+  }, [userName, validUserName]);
+
+  return (
+    <ClickAwayListener
+      onClickAway={() => {
+        validUserName === false
+          ? setEditUserName(true)
+          : setEditUserName(false);
+      }}
+      mouseEvent={"onMouseDown"}
+    >
+      <div className="name_bar">
+        {editUserName || validUserName === false ? (
+          <TextField
+            autoFocus
+            value={userName}
+            onChange={(event) => {
+              event.preventDefault();
+              setUserName(event.target.value);
+            }}
+            onKeyDown={(event) => {
+              if (event.key === "Enter") {
+                event.preventDefault();
+                handlerSaveName();
+              }
+            }}
+            InputProps={{ sx: { height: 35 } }}
+          />
+        ) : (
+          <Typography
+            className="name_bar-typography"
+            sx={{ color: "black", textOverflow: "ellipsis" }}
+          >
+            {userName}
+          </Typography>
+        )}
+        <IconButton
+          color="black"
+          onClick={() =>
+            editUserName || validUserName === false
+              ? handlerSaveName()
+              : handlerEditName()
+          }
+        >
+          {editUserName || validUserName === false ? <Check /> : <Edit />}
+        </IconButton>
+      </div>
+    </ClickAwayListener>
+  );
 };
 
 export default NameBar;
